e755c5d8dad90c6f9bc7f1f7dbcc05117acfc7f6
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-07 22:10:39 +1000

    Added VHDLParsing to dependencies.

--- Package.swift
+++ Package.swift
@@ -3,27 +3,36 @@
 
 import PackageDescription
 
+/// The package definition.
 let package = Package(
     name: "TCTLParser",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other
+        // packages.
         .library(
             name: "TCTLParser",
-            targets: ["TCTLParser"]),
+            targets: ["TCTLParser"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+        .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package
+        // depends on.
         .target(
             name: "TCTLParser",
-            dependencies: []),
+            dependencies: [
+                .product(name: "VHDLParsing", package: "VHDLParsing")
+            ]
+        ),
         .testTarget(
             name: "TCTLParserTests",
-            dependencies: ["TCTLParser"]),
+            dependencies: ["TCTLParser"]
+        )
     ]
 )
